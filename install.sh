8b5ed6e94f2d7c34d556022572546e884a7d884c
Michael Chen <omxcodec@gmail.com>
2012-11-17 10:30:58 +0800

    check file /system/lib/libncurses.so exists, if no, push it

--- install.sh
+++ install.sh
@@ -8,4 +8,15 @@ adb push bashrc /system/etc/bash
 adb push bash /system/xbin
 adb push busybox /system/xbin
 
+exist=$(adb shell "if [ -f "/system/lib/libncurses.so" ]; then echo "YES"; else echo "NO"; fi")
+
+# exist len is 3!
+#echo $exist
+#len=`expr length $exist`
+#echo len=$len
+
+if [ ${exist:0:2} == "NO" ]; then
+adb push libncurses.so /system/lib
+fi
+
 adb shell sync
